caab59f29078b9189f5c8a2d8ddcfaab8e1f0443
dantecaniglia-maker <dantecaniglia@gmail.com>
2026-03-13 10:06:05 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'miocane-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) { return c.addAll(ASSETS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(ks) {
+      return Promise.all(ks.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      if (cached) return cached;
+      return fetch(e.request).then(function(res) {
+        var rc = res.clone();
+        caches.open(CACHE).then(function(c) { c.put(e.request, rc); });
+        return res;
+      }).catch(function() {
+        return caches.match('/index.html');
+      });
+    })
+  );
+});
