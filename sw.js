f506c1cab873d7ffdd57129bddce35e70f52dbd9
Dante Caniglia <dantecaniglia@gmail.com>
2026-04-26 15:24:20 +0200

    fix: prove mostra stato vuoto se no user v12.43

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ── CAMBIA QUESTO NUMERO AD OGNI DEPLOY ──
-const VERSION = '12.42';
+const VERSION = '12.43';
 const CACHE = 'miocane-' + VERSION;
 
 self.addEventListener('message', function(e){
